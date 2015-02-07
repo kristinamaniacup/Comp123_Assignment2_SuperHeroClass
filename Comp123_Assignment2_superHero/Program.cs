e3d51131c03b5c4f104ac8484ba29ba3a44c1dae
kristinamaniacup <kc_maniacup@hotmail.com>
2015-02-06 22:03:57 -0800

    Update: Program header, comments and overall edit of code

--- Comp123_Assignment2_superHero/Program.cs
+++ Comp123_Assignment2_superHero/Program.cs
@@ -1,7 +1,15 @@
-﻿/*Author Name: Kristina Maniacup
+﻿/* Author Name: Kristina Maniacup
  * Date Last Modified: Friday, February 6, 2015
- * Program Description: 
+ * Program Description: This program is to demonstrate building and implementing a superHero class.
+ *                      The superHero class generates 3 different super powers for the Super Hero "Mom" and makes sure a power isn't repeated.
+ *                      The main program will call the showPowers method from the SuperHero class as well as the fight method from the Hero class.
  * 
+ * Revision History:
+ * 
+ * 02/03/15: Created the "skeleton" on the project (outlined methods and constructor)
+ * 02/04/15: Created generateRandomNum method
+ * 02/05/15: Created generateRandomPowers method and showPowers method
+ * 02/06/15: Fixed up showPowers to make it look pretty, add comments and review code
  * 
  * 
  */using System;
@@ -17,15 +25,15 @@ namespace Comp123_Assignment2_superHero
         static void Main(string[] args)
         {
             Console.WriteLine();
-            Console.WriteLine("~~~~~~~~~~~~~ SuperHero Class ~~~~~~~~~~~~~~");
-            Console.WriteLine();
+            Console.WriteLine("~~~~~~~~~~~~~~ SUPERHERO CLASS ~~~~~~~~~~~~~");
             SuperHero mySuperHero = new SuperHero("Mom");
             mySuperHero.showPowers();
             Console.WriteLine();
-            Console.WriteLine("~~~~~~~~~~~~~~~ Hero Class ~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("~~~~~~~~~~~~~~~ HERO CLASS ~~~~~~~~~~~~~~~~~");
             Console.WriteLine();
             Hero myHero = new Hero("Dad");
             myHero.fight();
+            Console.WriteLine();
             Console.WriteLine("********* Press any key to exit. ***********");
             Console.ReadKey();
         }
--- Comp123_Assignment2_superHero/SuperHero.cs
+++ Comp123_Assignment2_superHero/SuperHero.cs
@@ -8,7 +8,7 @@ namespace Comp123_Assignment2_superHero
 {
     class SuperHero : Hero
     {
-        //PRIVATE PROPERTIES +++++++
+        //PRIVATE PROPERTIES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         private string[] superPowers = {"Invisibility", "Super Strength", "Flight", "Super Speed", "Telekinesis", "Telepathy" };
         private string[] finalSuperPowers = new string[3];
 
@@ -16,30 +16,33 @@ namespace Comp123_Assignment2_superHero
 
         private int randomPower;
 
-        //CONSTRUCTOR +++++++++++++
+        //CONSTRUCTOR +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         public SuperHero(string name) : base(name)
         {
             generateRandomPowers(); //displays random powers
         }
 
-        //PRIVATE METHODS +++++++++++
+
+        //This method generates 3 different powers for the superHero
+        //PRIVATE METHODS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         private void generateRandomPowers()
         {
             int power = 0;
             while (power < finalSuperPowers.Length)
             {
-                randomPower = generateRandomNum(rnd);
-                if (superPowers[randomPower] != "Opps! Unavailable. Re-roll.")
+                randomPower = generateRandomNum(rnd); 
+                if (superPowers[randomPower] != "Opps! Unavailable. Re-roll.") //if the power generated is available then..
                 {
-                    finalSuperPowers[power] = superPowers[randomPower];
+                    finalSuperPowers[power] = superPowers[randomPower]; //apply that power to the finalSuperPower
 
-                    superPowers[randomPower] = "Opps! Unavailable. Re-roll.";
+                    superPowers[randomPower] = "Opps! Unavailable. Re-roll."; //but if the power generated IS unavailable then exit loop and "re-roll"
 
                     power++;
                 }
             }
         }
 
+        //Method that generates a random number in relation to the string array items and is used in the generateRandomPowers
         private static int generateRandomNum(Random rnd)
         {
             int randomPower;
@@ -48,14 +51,18 @@ namespace Comp123_Assignment2_superHero
             return randomPower;
         }
 
-        //PUBLIC METHODS ++++++++++++
+        //PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         public void showPowers()
         {
             Console.WriteLine("********************************************");
-            Console.WriteLine("************* Super Powers *****************");
+            Console.WriteLine("                Super Powers                ");
+            Console.WriteLine("********************************************");
             Console.WriteLine("First Super Power is: {0}", finalSuperPowers[0]);
+            Console.WriteLine();
             Console.WriteLine("Second Super Power is: {0}", finalSuperPowers[1]);
+            Console.WriteLine();
             Console.WriteLine("Third Super Power is: {0}", finalSuperPowers[2]);
+            Console.WriteLine("********************************************");
             Console.WriteLine("********************************************");
         }
     }
